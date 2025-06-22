f083ad8d62caadab3f9b0a58d80ca93bc67e49b2
Phạm Quốc Đạt <pham.dat292@gmail.com>
2025-06-22 23:25:09 +0200

    Remove unused font typeface

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,29 +17,62 @@ import './globals.css';
 
 const beVietnamPro = Be_Vietnam_Pro({
   variable: '--font-be-vietnam-pro',
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  weight: [
+    // '100', // font-thin
+    // '200', // font-extralight
+    // '300', // font-light
+    '400', // font-normal
+    // '500', // font-medium
+    '600', // font-semibold
+    // '700', // font-bold
+    // '800', // font-extrabold
+    '900', // font-black
+  ],
   style: ['normal', 'italic'],
   subsets: ['latin'],
 });
 
 const playfair = Playfair({
   variable: '--font-playfair',
-  weight: ['400', '500', '600', '700', '800', '900'],
+  weight: [
+    '400', // font-normal
+    // '500', // font-medium
+    '600', // font-semibold
+    // '700', // font-bold
+    // '800', // font-extrabold
+    // '900', // font-black
+  ],
   style: ['normal', 'italic'],
   subsets: ['latin'],
 });
 
 const playfairDisplay = Playfair_Display({
   variable: '--font-playfair-display',
-  weight: ['400', '500', '600', '700', '800'],
-  style: ['normal', 'italic'],
+  weight: [
+    '400', // font-normal
+    // '500', // font-medium
+    '600', // font-semibold
+    // '700', // font-bold
+    // '800', // font-extrabold
+  ],
+  style: [
+    'normal',
+    //'italic'
+  ],
   subsets: ['latin'],
 });
 
 const playfairDisplaySc = Playfair_Display_SC({
   variable: '--font-playfair-display-sc',
-  weight: ['400', '700', '900'],
-  style: ['normal', 'italic'],
+  weight: [
+    '400', // font-normal
+    //'700', // font-bold
+    // '900', // font-black
+  ],
+  style: [
+    'normal',
+    //'italic'
+  ],
   subsets: ['latin', 'vietnamese'],
 });
 
